23a5b5d88458ea49d7fe44e607ae023560e6bf97
olgapopova <olga.popova@ivkhk.ee>
2015-10-12 16:30:38 +0300

    обновление 12.10

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,23 +1,36 @@
 <?php
+function render_template($path, array $args){
+	extract($args);
+	ob_start();
+	require $path;
+	$html=ob_get_clean();
+	return $html;
+}
 function list_action()
 {
 	$sql="SELECT * FROM pages ORDER BY id DESC";
 	$rows=get_all_rows($sql);
-	require "view/list.php";
+	$html=render_template("view/list.php",array ('rows'=>$rows));
+	return $html;
+	//require "view/list.php";
 }
 function admin_action()
 {
 	$sql="SELECT * FROM pages ORDER BY id DESC";
 	$rows=get_all_rows($sql);
-	require "view/admin.php";
+	$html=render_template("view/admin.php",array ('rows'=>$rows));
+	return $html;
+	//require "view/admin.php";
 }
 function add_action()
 {
-	$row=add_row();
+	add_row();//$row=
 	
 	$sql="SELECT * FROM pages ORDER BY id DESC";
 	$rows=get_all_rows($sql);
-	require "view/admin.php";
+	$html=render_template("view/admin.php",array ('rows'=>$rows));
+	return $html;
+	//require "view/admin.php";
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors',1);
+error_reporting(E_ALL ^ E_DEPRECATED);
+ini_set('display_errors','On');
+include "controllers/controller.php";
 include "model.php";
 
 $uri=$_SERVER['REQUEST_URI'];
@@ -9,15 +10,15 @@ echo "uri=$uri";
 echo '<br>SERVER[PHP_SELF]='.$_SERVER['PHP_SELF'];
 
 
-if ($uri=='/mysite/index.php' || $uri=='/mysite/')
+if ($uri=='/mysite/index.php' || $uri=='/mysite')
 {
 	$response=list_action();
 } 
-elseif($uri=='/mysite/index.php/admin' || $uri=='/mysite/')
+elseif($uri=='/mysite/index.php/admin' || $uri=='/mysite')
 {
 	$response=admin_action();
 }
-elseif($uri=='/mysite/index.php/add' || $uri=='/mysite/')
+elseif($uri=='/mysite/index.php/add' || $uri=='/mysite')
 {
 	$response=add_action();
 }
